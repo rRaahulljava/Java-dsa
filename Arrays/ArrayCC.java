a3503af1b468ad582e609492c023184e4fdf6604
rRaahulljava <rahjket@gmail.com>
2024-04-03 02:33:15 +0530

    p

--- Arrays/ArrayCC.java
+++ Arrays/ArrayCC.java
@@ -4,16 +4,16 @@ import java.util.Scanner;
 
 public class ArrayCC {
    public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
+    Scanner in = new Scanner(System.in);
     int marks[] = new int[50];
     int number[] = {1, 3, 4};
 
     String fruits[] = {"apple", "mango"};
     System.out.println();
 
-    marks[0] = sc.nextInt();
-    marks[1] = sc.nextInt();
-    marks[2] = sc.nextInt();
+    marks[0] = in.nextInt();
+    marks[1] = in.nextInt();
+    marks[2] = in.nextInt();
 
     System.out.println("phy :" + marks[0]);
     System.out.println("math :" + marks[1]);
